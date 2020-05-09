d06b55c623819d201e23586b15e5e20be72c1595
Sun <dkssud940@gmail.com>
2020-05-09 22:49:01 +0900

    Codeup 예제 1015, 1017

---
+++ JavaPrj/src/Codeup1015.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Codeup1015 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		double a = scn.nextDouble();
+		double b = (Math.round(a*100))/100.0;
+		System.out.printf("%.2f", b);
+	}
+}
+
