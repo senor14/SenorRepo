74ed0d403dfd51a4885fb1eacea8a3de0b70cb6f
Sun <dkssud940@gmail.com>
2020-05-10 20:46:09 +0900

    Codeup 예제 1021

---
+++ JavaPrj/src/Codeup1021.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Codeup1021 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String a = scn.nextLine();
+		System.out.println(a);
+
+	}
+
+}
