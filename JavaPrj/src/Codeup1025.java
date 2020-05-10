8d88446a5eb3cd8c200eb6c485817123090fd8c0
Sun <dkssud940@gmail.com>
2020-05-10 20:52:30 +0900

    Codeup 예제 1025

---
+++ JavaPrj/src/Codeup1025.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Codeup1025 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String a = scn.next();
+		String[] b = a.split("");
+		for(int i=0; i<b.length; i++) {
+			System.out.println("[" + Integer.parseInt(b[i])*10000/(int)Math.pow(10, i) + "]");	
+		}
+		
+	}
+
+}
+
