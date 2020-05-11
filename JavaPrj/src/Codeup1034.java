72dc7656bd8bc22e803f8953e7e34598d716b97b
Senor <dkssud940@gmail.com>
2020-05-12 08:03:45 +0900

    Codeup 예제 1034

--- JavaPrj/src/Codeup1034.java
+++ JavaPrj/src/Codeup1034.java
@@ -1,10 +1,11 @@
 import java.util.*;
-public class Codeup1033 {
+public class Codeup1034 {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		int a = scn.nextInt();
-		System.out.printf("%H",a);
+		String a = scn.nextLine();
+		int b = Integer.valueOf(a, 8);
+		System.out.println(b);
 	}
 
 }
