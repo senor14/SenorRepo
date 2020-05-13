cbde880371d3dfd77aecb5c45103d874a52bc117
Senor <dkssud940@gmail.com>
2020-05-14 08:03:18 +0900

    Codeup 예제 1042

--- JavaPrj/src/Codeup1041.java
+++ JavaPrj/src/Codeup1041.java
@@ -1,16 +1,13 @@
 import java.util.*;
-
 public class Codeup1041 {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		String a = scn.next();
-		String[] b = a.split("");
-		for(int i=0; i<b.length; i++) {
-			System.out.println("[" + Integer.parseInt(b[i])*10000/(int)Math.pow(10, i) + "]");	
-		}
-		
+		char a = scn.nextLine().charAt(0);
+		int b = (int)a;
+		a = (char)(b+1);
+		System.out.println(a);
+
 	}
 
 }
-
