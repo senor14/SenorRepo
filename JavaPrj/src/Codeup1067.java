d391013212889139c35507b4ed0082aa0b870404
Senor <dkssud940@gmail.com>
2020-05-19 08:11:51 +0900

    Codeup 예제 1067

---
+++ JavaPrj/src/Codeup1067.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Codeup1067 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		long a = scn.nextInt();
+		if(a<0) {
+			System.out.println("minus");
+			if(a%2==0) {
+				System.out.println("even");
+			}else {
+				System.out.println("odd");
+			}
+		}else if(a>0) {
+			System.out.println("plus");
+			if(a%2==0) {
+				System.out.println("even");
+			}else {
+				System.out.println("odd");
+			}
+		}
+	}
+
+}
+
