c5c08bb36f02efc9b28ba4be116e640bf83cb441
Senor <dkssud940@gmail.com>
2020-05-18 09:43:57 +0900

    Codeup 예제 1061

---
+++ JavaPrj/src/Codeup1061.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Codeup1061 {
+
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		
+		long a = scn.nextInt();
+		long b = scn.nextInt();
+		
+		System.out.println(a|b);
+
+	}
+
+}
+
