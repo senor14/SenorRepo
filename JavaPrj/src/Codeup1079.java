42ffd7a4be5e843c2ea507506c7e632baf02bbab
Senor <dkssud940@gmail.com>
2020-05-21 08:32:23 +0900

    Codeup 예제 1079

---
+++ JavaPrj/src/Codeup1079.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Codeup1079 {
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		while(true) {
+			String a = scn.next();
+			if(a.equals("q")) {
+				System.out.println(a);
+				break;
+			}
+			System.out.println(a);
+		}
+		
+	}
+
+}
+
