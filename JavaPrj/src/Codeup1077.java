8d2b817c0a00454632a4aa39545563f6835ef456
Senor <dkssud940@gmail.com>
2020-05-21 08:31:10 +0900

    Codeup 예제 1077

---
+++ JavaPrj/src/Codeup1077.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Codeup1077 {
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		long a = scn.nextInt();
+		long b = 0;
+		do {
+			System.out.println(b);
+			b++;
+		}while(b<=a);
+		
+	}
+
+}
+
