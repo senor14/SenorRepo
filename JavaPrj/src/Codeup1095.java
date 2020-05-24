a27b59ee1f64934da9e2c7778402d67b983e7013
Senor <dkssud940@gmail.com>
2020-05-25 08:58:47 +0900

    Codeup 예제 1095

---
+++ JavaPrj/src/Codeup1095.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Codeup1095 {
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] array1 = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			int m = scn.nextInt();
+			array1[i] = m;
+		}
+		int m = array1[0];
+		for(int j=0; j<n; j++) {
+			if(m > array1[j]) {
+				m = array1[j];
+			}
+		}
+		System.out.println(m);
+	}
+
+}
