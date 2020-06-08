6909b608feaa669625671297883c6a8880f8eb16
Senor <dkssud940@gmail.com>
2020-06-09 08:56:04 +0900

    [레벨1]약수의 합

---
+++ JavaPrj/src/algorithm/programmers/level1/Measure_Sum.java
@@ -0,0 +1,26 @@
+package algorithm.programmers.level1;
+
+public class Measure_Sum {
+
+	public static void main(String[] args) {
+
+		Measuresum ms = new Measuresum();
+		int n1 = 12;
+		int result = ms.solution(n1);
+		System.out.println(result);
+
+	}
+
+}
+
+class Measuresum {
+	  public int solution(int n) {
+	      int answer = 0;
+	      for(int i=1; i<=n; i++) {
+	          if(n%i==0) {
+	              answer += i;
+	          }
+	      }
+	      return answer;
+	  }
+}
