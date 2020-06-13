1f8cd135bb6d40bb305bcbc3bffa809cff3a85f4
Senor <dkssud940@gmail.com>
2020-06-13 17:54:46 +0900

    [레벨1]정수 내림차순으로 배치하기

---
+++ JavaPrj/src/algorithm/programmers/level1/Integer_Descending.java
@@ -0,0 +1,27 @@
+package algorithm.programmers.level1;
+
+import java.util.*;
+
+public class Integer_Descending {
+
+	public static void main(String[] args) {
+
+		Integerdescending id = new Integerdescending();
+		long n1 = 118372;
+		long result = id.solution(n1);
+		System.out.println(result);
+
+	}
+
+}
+
+class Integerdescending {
+  public long solution(long n) {
+      String a = "" + n;
+      String[] b = a.split("");
+      Arrays.sort(b);
+      Collections.reverse(Arrays.asList(b));
+      long c = Long.parseLong(String.join("",b));
+      return c;
+  }
+}
