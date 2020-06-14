a55d6bfa5381e8c87d24d4b2a08af80a01f0d34f
Senor <dkssud940@gmail.com>
2020-06-14 11:31:59 +0900

    [레벨1]정수 제곱근 판별

---
+++ JavaPrj/src/algorithm/programmers/level1/Integer_Square_Root.java
@@ -0,0 +1,25 @@
+package algorithm.programmers.level1;
+
+public class Integer_Square_Root {
+
+	public static void main(String[] args) {
+		Integersquareroot isr = new Integersquareroot();
+		long n1 = 121;
+		long result = isr.solution(n1);
+		System.out.println(result);
+	}
+}
+
+class Integersquareroot {
+    public long solution(long n) {
+        long s = (long)Math.sqrt(n);
+        if(s * s == n)
+        {
+            return (long)Math.pow(s+1,2);
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
