c14e2630468d11cb8b73f8967506d26549dce7a4
Senor <dkssud940@gmail.com>
2020-06-22 08:36:15 +0900

    [레벨1]행렬의 덧셈

---
+++ JavaPrj/src/algorithm/programmers/level1/Plus_Matrix.java
@@ -0,0 +1,30 @@
+package algorithm.programmers.level1;
+
+import java.util.Arrays;
+
+public class Plus_Matrix {
+
+	public static void main(String[] args) {
+
+		Plusmatrix pm = new Plusmatrix();
+		int[][] arr11 = {{1,2},{2,3}};
+		int[][] arr22 = {{3,4},{5,6}};
+		int[][] result = pm.solution(arr11, arr22);
+
+	}
+
+}
+
+class Plusmatrix {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for(int i = 0; i < arr1.length; i++)
+        {
+            for(int j = 0; j < arr1[0].length; j++)
+            {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
