a15251ccd990e5d3acb1aab8e982d434f70e3d47
Senor <dkssud940@gmail.com>
2020-06-24 00:21:59 +0900

    [레벨1]직사각형 별찍기

--- JavaPrj/src/algorithm/programmers/level1/Rectangle_Star_12969.java
+++ JavaPrj/src/algorithm/programmers/level1/Rectangle_Star_12969.java
@@ -2,7 +2,7 @@ package algorithm.programmers.level1;
 
 import java.util.Scanner;
 
-public class Rectangle_Star {
+public class Rectangle_Star_12969 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
