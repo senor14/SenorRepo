43cf0c3e342c8b2ce4d6a81b70c06b7c8c5852da
Senor <dkssud940@gmail.com>
2020-07-08 14:50:41 +0900

    [백준]8진수_2진수_1212

---
+++ JavaPrj/src/algorithm/baekjoon/팔진수_이진수_1212.java
@@ -0,0 +1,47 @@
+package algorithm.baekjoon;
+
+import java.util.Scanner;
+
+public class 팔진수_이진수_1212 {
+
+	public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String[] eight = {"000","001","010","011","100","101","110","111"};
+        String s = sc.nextLine();
+        boolean start = true;
+        if (s.length() == 1 && s.charAt(0) == '0') {
+            System.out.print(0);
+        }
+        for (int i=0; i<s.length(); i++)
+        {
+            int n = s.charAt(i) - '0';
+            if (start == true && n < 4)
+            {
+                if (n == 0)
+                {
+                    continue;
+                }
+                else if (n == 1)
+                {
+                    System.out.print("1");
+                }
+                else if (n == 2)
+                {
+                    System.out.print("10");
+                }
+                else if (n == 3)
+                {
+                    System.out.print("11");
+                }
+                start = false;
+            }
+            else
+            {
+                System.out.print(eight[n]);
+                start = false;
+            }
+        }
+        System.out.println();
+    }
+
+}
