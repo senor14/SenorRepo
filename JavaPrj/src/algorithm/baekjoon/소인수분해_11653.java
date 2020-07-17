19b9147ebee61bfe3b9d0177551e2f50d003b613
Senor <dkssud940@gmail.com>
2020-07-18 01:04:28 +0900

    [백준]소인수분해_11653

---
+++ JavaPrj/src/algorithm/baekjoon/소인수분해_11653.java
@@ -0,0 +1,20 @@
+package algorithm.baekjoon;
+import java.util.*;
+public class 소인수분해_11653 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 2; i*i <= n; i++)
+		{
+			while (n%i == 0)
+			{
+				System.out.println(i);
+				n /= i;
+			}
+		}
+		if (n > 1)
+		{
+			System.out.println(n);
+		}
+	}
+}
