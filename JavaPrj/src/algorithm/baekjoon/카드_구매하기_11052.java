6301dac7ec24cdd524ad7abb5f1b1551ee4cdf5b
Senor <dkssud940@gmail.com>
2020-07-28 15:46:10 +0900

    [백준]카드_구매하기_11052

---
+++ JavaPrj/src/algorithm/baekjoon/카드_구매하기_11052.java
@@ -0,0 +1,21 @@
+package algorithm.baekjoon;
+import java.util.*;
+public class 카드_구매하기_11052 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n+1];
+		for (int i=1; i<=n; i++) {
+			a[i] = sc.nextInt();
+		}
+		int[] d = new int[n+1];
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<=i; j++) {
+				if (d[i] < d[i-j] + a[j]) {
+					d[i] = d[i-j] + a[j];
+				}
+			}
+		}
+		System.out.println(d[n]);
+	}
+}
