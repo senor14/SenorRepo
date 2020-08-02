ffe2d992f15659f3fd2db3e367bde25df8e10a41
Senor <dkssud940@gmail.com>
2020-08-03 00:03:23 +0900

    [백준]날짜_계산_1476

---
+++ JavaPrj/src/algorithm/baekjoon/날짜_계산_1476.java
@@ -0,0 +1,29 @@
+package algorithm.baekjoon;
+import java.util.*;
+public class 날짜_계산_1476 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int E = sc.nextInt();
+		int S = sc.nextInt();
+		int M = sc.nextInt();
+		int e=1, s=1, m=1;
+		for (int i=1;; i++) {
+			if (e == E && s == S && m == M) {
+				System.out.println(i);
+				break;
+			}
+			e += 1;
+			s += 1;
+			m += 1;
+			if (e == 16) {
+				e = 1;
+			}
+			if (s == 29) {
+				s = 1;
+			}
+			if (m == 20) {
+				m = 1;
+			}
+		}
+	}
+}
