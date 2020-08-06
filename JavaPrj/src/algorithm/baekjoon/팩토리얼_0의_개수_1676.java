b825d06ef88f01f5ce1b8fb44f4524a36730e6e2
Senor <dkssud940@gmail.com>
2020-08-07 00:25:20 +0900

    [백준]팩토리얼_0의_개수_1676

--- JavaPrj/src/algorithm/baekjoon/팩토리얼_0의_개수_1676.java
+++ JavaPrj/src/algorithm/baekjoon/팩토리얼_0의_개수_1676.java
@@ -1,9 +1,6 @@
 package algorithm.baekjoon;
-
 import java.util.Scanner;
-
 public class 팩토리얼_0의_개수_1676 {
-
 	public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
