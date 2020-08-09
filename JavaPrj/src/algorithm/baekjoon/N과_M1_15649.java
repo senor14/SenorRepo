dcf056f4f34068818b943330c38833c27c332585
Senor <dkssud940@gmail.com>
2020-08-09 15:07:00 +0900

    [백준]N과_M1_15649

---
+++ JavaPrj/src/algorithm/baekjoon/N과_M1_15649.java
@@ -0,0 +1,29 @@
+package algorithm.baekjoon;
+import java.util.*;
+public class N°ú_M1_15649 {
+	static boolean[] c = new boolean[10];
+	static int[] a = new int[10];
+	static void go(int index, int n, int m) {
+		if (index == m) {
+			for (int i=0; i<m; i++) {
+				System.out.print(a[i]);
+				if (i != m-1) System.out.print(" ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i=1; i<=n; i++) {
+			if (c[i]) continue;
+			c[i] = true;
+			a[index] = i;
+			go(index+1, n, m);
+			c[i] = false;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		go(0,n,m);
+	}
+}
