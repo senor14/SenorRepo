a84feebafa8c04d947a4caa287cb3e10b82f7b7a
Senor <dkssud940@gmail.com>
2020-08-15 13:04:15 +0900

    [백준]카잉_달력_6064

---
+++ JavaPrj/src/algorithm/baekjoon/카잉_달력_6064.java
@@ -0,0 +1,27 @@
+package algorithm.baekjoon;
+import java.util.*;
+import java.io.*;
+public class Ä«À×_´Þ·Â_6064 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.valueOf(bf.readLine());
+		while (t-- > 0) {
+			String[] line = bf.readLine().split(" ");
+			int m = Integer.valueOf(line[0]);
+			int n = Integer.valueOf(line[1]);
+			int x = Integer.valueOf(line[2])-1;
+			int y = Integer.valueOf(line[3])-1;
+			boolean ok = false;
+			for (int k=x; k<(n*m); k+=m) {
+				if (k%n == y) {
+					System.out.println(k+1);
+					ok = true;
+					break;
+				}
+			}
+			if (!ok) {
+				System.out.println(-1);
+			}
+		}
+	}
+}
