9151322d783612a7e1d34d8b311148930e6ae7df
Senor <dkssud940@gmail.com>
2020-08-17 00:10:31 +0900

    [백준]N과_M7_15655

---
+++ JavaPrj/src/algorithm/baekjoon/N과_M7_15655.java
@@ -0,0 +1,33 @@
+package algorithm.baekjoon;
+import java.util.*;
+public class N°ú_M7_15655 {
+	static int[] a = new int[10];
+	static int[] num = new int[10];
+	static StringBuilder go(int index, int n, int m) {
+		if (index == m) { 
+			StringBuilder sb = new StringBuilder();
+			for (int i=0; i<m; i++) {
+				sb.append(num[a[i]]);
+				if (i != m-1) sb.append(" ");
+			}
+			sb.append("\n");
+			return sb;
+		}
+		StringBuilder ans = new StringBuilder();
+		for (int i=0; i<n; i++) {
+			a[index] = i;
+			ans.append(go(index+1, n, m));
+		}
+		return ans;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			num[i] = sc.nextInt();
+		}
+		Arrays.sort(num, 0, n);
+		System.out.print(go(0,n,m));
+	}
+}
