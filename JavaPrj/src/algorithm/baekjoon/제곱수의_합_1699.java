3a9bcdc271e245d4f61ede5a05dcc97e174b66b6
Senor <dkssud940@gmail.com>
2020-08-21 08:39:47 +0900

    [백준]제곱수의_합_10973

---
+++ JavaPrj/src/algorithm/baekjoon/제곱수의_합_1699.java
@@ -0,0 +1,18 @@
+package algorithm.baekjoon;
+import java.util.*;
+public class Á¦°ö¼öÀÇ_ÇÕ_1699 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] d = new int[n+1];
+		for (int i=1; i<=n; i++) {
+			d[i] = i;
+			for (int j=1; j*j<=i; j++) {
+				if (d[i] > d[i-j*j] + 1) {
+					d[i] = d[i-j*j] + 1;
+				}
+			}
+		}
+		System.out.println(d[n]);
+	}
+}
