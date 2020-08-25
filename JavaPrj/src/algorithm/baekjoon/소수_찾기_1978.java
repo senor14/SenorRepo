98178241cdebdb27a233a36879566b43ff7cf0fc
Senor <dkssud940@gmail.com>
2020-08-26 00:03:17 +0900

    [백준]소수_찾기_1978

---
+++ JavaPrj/src/algorithm/baekjoon/소수_찾기_1978.java
@@ -0,0 +1,36 @@
+package algorithm.baekjoon;
+
+import java.util.*;
+public class 소수_찾기_1978 {
+    public static boolean is_prime(int x) {
+        if (x <= 1)
+        {
+            return false;
+        }
+        else if (x == 2)
+        {
+            return true;
+        }
+        for (int i=2; i*i <= x; i++)
+        {
+            if (x % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = 0;
+        while (n-- > 0)
+        {
+            if (is_prime(sc.nextInt()))
+            {
+                ans += 1;
+            }
+        }
+        System.out.println(ans);
+    }
+}
